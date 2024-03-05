8fde36fccaed6142a60f1a0752ed9181fe65913e
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-05 15:59:18 +0000

    Modify inside the function taskBlock so that the variables aren’t overwritten inside the conditional block.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = false;
+    const task2 = true;
+    return [task, task2];
   }
-
+  const task = false;
+  const task2 = true;
   return [task, task2];
 }
