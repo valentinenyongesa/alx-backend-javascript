4de67ce86306da49f1de509f28a816b3259fb75f
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-08 02:21:26 +0000

    Import the ClassRoom class from 0-classroom.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom.js';
+
+// Function to initialize rooms
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const rooms = sizes.map(size => new ClassRoom(size));
+  return rooms;
+}
+
+export default initializeRooms;
