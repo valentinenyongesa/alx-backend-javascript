96cd73b81e542dd14fc01c5777ba500f28b982bd
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-12 01:50:38 +0000

    Create a function named getListStudents that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+// 0-get_list_students.js
+
+function getListStudents() {
+  // Array of objects containing student information
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+
+  return students;
+}
+
+export default getListStudents;
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,4 @@
+0x03. ES6 data manipulation
+This project covers various concepts that include:
+1. JavaScript
+2. ES6
