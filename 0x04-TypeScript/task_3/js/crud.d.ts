ff77117019fc2cf4dec25e518fcc7476027fc38f
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-25 02:10:16 +0000

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+// crud.d.ts
+import { RowID, RowElement } from './interface';
+
+export declare function insertRow(row: RowElement): RowID;
+export declare function deleteRow(rowId: RowID): void;
+export declare function updateRow(rowId: RowID, row: RowElement): RowID;
