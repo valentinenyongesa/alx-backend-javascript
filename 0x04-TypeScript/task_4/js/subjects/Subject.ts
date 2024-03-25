31e6c57e0db3f81ee5b006bef4775bf3d5181237
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-25 02:23:31 +0000

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+// Subject.ts
+namespace Subjects {
+    export class Subject {
+        teacher: Teacher;
+
+        setTeacher(teacher: Teacher): void {
+            this.teacher = teacher;
+        }
+    }
+}
