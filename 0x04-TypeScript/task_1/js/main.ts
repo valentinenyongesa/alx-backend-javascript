d6b77c1c795db833b413c4fcc6e837795fba3fad
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-25 01:38:32 +0000

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,14 +1,32 @@
-interface Directors extends Teacher {
-    numberOfReports: number;
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
 }
 
-// Example usage
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: true,
-    location: 'London',
-    numberOfReports: 17
-};
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
 
-console.log(director1);
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage:
+const student1 = new StudentClass({ firstName: 'Alice', lastName: 'Smith' });
+console.log(student1.workOnHomework()); // Output: Currently working
+console.log(student1.displayName());    // Output: Alice
