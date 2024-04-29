804db0c1ba531b1a0d66155b99ea69acfc767d25
root <valentinenyongesa94@gmail.com>
2024-04-29 08:20:38 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+// 0-console.js
+
+/**
+ * Prints the given message to the standard output (STDOUT).
+ * @param {string} message The message to be displayed.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,7 @@
+0x05. NodeJS Basics
+This project covers the following concepts:
+1. Back-end
+2. JavaScript
+3. ES6
+4. NodeJS
+5. ExpressJS
