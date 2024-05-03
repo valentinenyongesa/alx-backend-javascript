2f22a4f13ae59bab02181378425a0b2792415496
root <valentinenyongesa94@gmail.com>
2024-05-03 01:02:28 +0000

    Creating spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,22 @@
-const { expect } = require('chai');
+// 3-payment.test.js
 const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-  it('should call Utils.calculateNumber with the correct arguments and log the result', function() {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(120);
-    const consoleSpy = sinon.spy(console, 'log');
+describe('sendPaymentRequestToApi function', () => {
+    it('should call Utils.calculateNumber with correct arguments and log the result', () => {
+        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+        const consoleLogSpy = sinon.spy(console, 'log');
 
-    sendPaymentRequestToApi(100, 20);
+        sendPaymentRequestToApi(100, 20);
 
-    expect(calculateNumberStub.calledWithExactly('SUM', 100, 20)).to.be.true;
-    expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
+        expect(calculateNumberSpy.calledOnce).to.be.true;
+        expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+        expect(consoleLogSpy.calledOnce).to.be.true;
+        expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
 
-    calculateNumberStub.restore();
-    consoleSpy.restore();
-  });
+        calculateNumberSpy.restore();
+        consoleLogSpy.restore();
+    });
 });
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,18 +1,21 @@
 // utils.js
-
 const Utils = {
-  calculateNumber(type, a, b) {
-    const numA = Number(a);
-    const numB = Number(b);
-    if (type === 'SUM') {
-      return Math.round(numA) + Math.round(numB);
-    } else if (type === 'SUBTRACT') {
-      return Math.round(numA) - Math.round(numB);
-    } else if (type === 'DIVIDE') {
-      if (Math.round(numB) === 0) return 'Error';
-      return Math.round(numA) / Math.round(numB);
+    calculateNumber(type, a, b) {
+        switch (type) {
+            case 'SUM':
+                return Math.round(a) + Math.round(b);
+            case 'SUBTRACT':
+                return Math.round(a) - Math.round(b);
+            case 'DIVIDE':
+                if (Math.round(b) === 0) {
+                    return 'Error';
+                } else {
+                    return Math.round(a) / Math.round(b);
+                }
+            default:
+                throw new Error('Invalid operation type');
+        }
     }
-  }
 };
 
-export default Utils;
+module.exports = Utils;
